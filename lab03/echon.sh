92581e9824a3840cb1e51458ed73589d0b9145e6
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-08 10:10:51 +1100

    finish question2

--- lab03/echon.sh
+++ lab03/echon.sh
@@ -14,21 +14,22 @@ fi
 
 num=$1
 str=$2
-# Use bc can check the input is number or not
+# Use bc can check whether the input is a string or not
 # number use bc is still itself but string use bc is 0
 # Source: https://stackoverflow.com/questions/31034540/correct-usage-of-bc-in-a-shell-script
 if test $(echo $num | bc) != $num  # if test "..." != "..."
 then 
     echo "./echon.sh: argument 1 must be a non-negative integer" 1>&2
-    exit
-fi
-
-if [ $num -lt 0 ]
-then
+    exit 1
+# check the number which is negative
+elif test $num -lt 0 
+then 
     echo "./echon.sh: argument 1 must be a non-negative integer" 1>&2
-    exit
+    exit 1
 fi
 
+
+# using a counter to check the time of print
 counter=0
 while test $counter -lt $num 
 do
