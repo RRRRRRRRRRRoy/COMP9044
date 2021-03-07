bc44ac373139dba026a96560e374c01d759e9657
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-08 09:31:46 +1100

    Question 2 check numbers of integer

---
+++ lab03/balancing_numbers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while read line
+do
+    echo $line | tr "[0-4]|[6-9]" '[<<<<<]|[>>>>]'
+done
---
+++ lab03/echon.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if test $# -ne 2
+then
+    echo "Usage: ./echon.sh <number of lines> <string>" 1>&2
+fi
+
