812f35f8f437ca4870019099f35f74a4b60d023b
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-08 09:57:11 +1100

    check the number of input and negative

--- lab03/echon.sh
+++ lab03/echon.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
 
-if test $# -ne 2
+# Check the number of input 
+# <number of lines> <string> number is 2
+if test $# -le 0
 then
     echo "Usage: ./echon.sh <number of lines> <string>" 1>&2
+    exit 1
+elif test $# -gt 2
+then
+    echo "Usage: ./echon.sh <number of lines> <string>" 1>&2
+    exit 1
+fi
+
+num=$1
+str=$2
+# Use bc can check the input is number or not
+# number use bc is still itself but string use bc is 0
+# Source: https://stackoverflow.com/questions/31034540/correct-usage-of-bc-in-a-shell-script
+if test $(echo num|bc)!=$1
+then 
+    echo "./echon.sh: argument 1 must be a non-negative integer" 1>&2
+    exit 1
+elif test $num -lt 0
+then
+    echo "./echon.sh: argument 1 must be a non-negative integer" 1>&2
+    exit 1
 fi
 
