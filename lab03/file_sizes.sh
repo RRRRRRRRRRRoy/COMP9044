60f22440ba2102f2217b00018b68c4d43e681ff4
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-08 13:11:40 +1100

    write the if condition of Q3

--- lab03/file_sizes.sh
+++ lab03/file_sizes.sh
@@ -13,5 +13,15 @@ do
         line_size=$(echo $line | cut -d' ' -f1)
         file_name=$(echo $line | cut -d' ' -f2)
         # Check the file lines and filename print
-        echo -n $line_size && echo -n ' ' && echo  $file_name
+        # echo -n $line_size && echo -n ' ' && echo  $file_name
+        if test $line_size -lt 10
+        then
+            :
+        elif test $line_size -ge 10 && test $line_size -lt 100
+        then
+            :
+        elif test $line_size -ge 100
+        then
+            :
+        fi
 done
