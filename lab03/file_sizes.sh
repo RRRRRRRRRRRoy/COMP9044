094dac030e7fa49a296e57a41eda0e604d06ae66
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-08 14:23:25 +1100

    test the array version in vlab

--- lab03/file_sizes.sh
+++ lab03/file_sizes.sh
@@ -3,6 +3,8 @@
 # COMP9041 Lab02 Exercise3 : File Sizes
 
 # Creating arrays for 3 kinds of files
+# Array operation in Shell
+# Source: https://stackoverflow.com/questions/1878882/arrays-in-unix-shell
 small_size=()
 medium_size=()
 large_size=()
@@ -14,29 +16,31 @@ file_counter=0
 
 wc -l $(ls *) | while read line
 do
-        size=$(echo $line | cut -d' ' -f1)
+        file_lines=$(echo $line | cut -d' ' -f1)
         file_name=$(echo $line | cut -d' ' -f2)
 
-        # Array operation in Shell
-        # Source: https://stackoverflow.com/questions/1878882/arrays-in-unix-shell
-        if test $size -lt 10 
+        if test $file_lines -lt 10 
         then
             # before start len is 0 so we can store data in the 1st place
             # Then move to the second place which index is current len is 1
             # In this way this array can be Scalable
-            index_s=${#small_size[*]}
-            small_size[$index_s]=$file_name
-        elif test $size -ge 10 && test $size -lt 100 
+            # Details in looping an array
+            # Source: https://stackoverflow.com/questions/8545943/shell-script-arrays
+            index_small=${#small_size[*]}
+            small_size[$index_small]=$file_name
+        elif test $file_lines -ge 10 && test $file_lines -lt 100 
         then 
-            index_m=${#medium_size[*]}
-            medium_size[$index_m]=$file_name
-        elif test $size -lt 1000
+            # size is used to do the if-condition
+            # name is the output --> name store to the array
+            index_medium=${#medium_size[*]}
+            medium_size[$index_medium]=$file_name
+        elif test $file_lines -ge100 && test $file_lines -lt 1000
         then 
-            index_l=${#large_size[*]}
-            large_size[$index_l]=$file_name
+            index_large=${#large_size[*]}
+            large_size[$index_large]=$file_name
         fi
 
-        # Counter move to next place
+        # Counter move to next file
         file_counter=$(($file_counter + 1))
 
         # checking whether go through all the current files in dir
