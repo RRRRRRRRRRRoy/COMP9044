496d65b85ce749e832d9b6f743df587de28066b6
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-08 19:28:37 +1100

    Q2

---
+++ test03/most_common_first_name.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cut -d'|' -f3,3 | cut -d' ' -f2,2 | sort | uniq -c | sort -n | tail -1 | sed "s/^.*[0-9] //g"
+
--- test03/student_first_names.sh
+++ test03/student_first_names.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cut -d'|' -f3 | egrep  "^, [a-zA-Z]* $" 
+cut -d'|' -f3,3 | cut -d' ' -f2,2 | sort | uniq -c | sort -n 
