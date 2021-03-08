cff05ef7ccc98d3e0bedac969f9f98fc1f563ad8
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-08 16:33:47 +1100

    test Q4

--- lab03/scraping_courses.sh
+++ lab03/scraping_courses.sh
@@ -6,18 +6,22 @@ then
     exit 1
 fi
 
-if [[ $(echo $1 | bc) -ne $1 ]] 2>/dev/null
+filename=$0
+input_year=$1
+input_code=$2
+
+if [[ $(echo $input_year | bc) -ne $input_year ]] 2>/dev/null
 then
-    echo "$0: argument 1 must be an integer between 2019 and 2021" 
+    echo "$filename: argument 1 must be an integer between 2019 and 2021" 
     exit 1
 
 
-elif test $1 -ge 2019 2>/dev/null && test $1 -le 2021 2>/dev/null
+elif test $input_year -ge 2019 2>/dev/null && test $input_year -le 2021 2>/dev/null
 then
     :
 
 else
-    echo "$0: argument 1 must be an integer between 2019 and 2021"
+    echo "$filename: argument 1 must be an integer between 2019 and 2021"
     exit 1
 fi
 
@@ -27,4 +31,6 @@ code=$2
 undergraduate="https://www.handbook.unsw.edu.au/api/content/render/false/query/+unsw_psubject.implementationYear:$year%20+unsw_psubject.studyLevel:undergraduate%20+unsw_psubject.educationalArea:$code*%20+unsw_psubject.active:1%20+unsw_psubject.studyLevelValue:ugrd%20+deleted:false%20+working:true%20+live:true/orderby/unsw_psubject.code%20asc/limit/10000/offset/0"
 postgraduate="https://www.handbook.unsw.edu.au/api/content/render/false/query/+unsw_psubject.implementationYear:$year%20+unsw_psubject.studyLevel:postgraduate%20+unsw_psubject.educationalArea:$code*%20+unsw_psubject.active:1%20+unsw_psubject.studyLevelValue:pgrd%20+deleted:false%20+working:true%20+live:true/orderby/unsw_psubject.code%20asc/limit/10000/offset/0"
 
-curl -sL $undergraduate $postgraduate |2041 jq '.contentlets[] |.code + " " + .title'| sed "s/\"//g" | sort |uniq
+curl -sL $undergraduate $postgraduate |2041 jq '.contentlets[] |.code + " " + .title'| tr "\"" "" | sort |uniq
+
+#  sed "s/\"//g"
