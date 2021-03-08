db801cd34f1b84fed17f194d0895fa61bb3987ff
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-08 19:36:58 +1100

    Q2

--- test03/most_common_first_name.sh
+++ test03/most_common_first_name.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cut -d'|' -f3 | cut -d' ' -f2 | sort | uniq -c | sort -n | tail -1| sed "s/^.*[0-9].//g"
+cut -d'|' -f3,3 | cut -d' ' -f2,2 | sort | uniq -c | sort -n | tail -1| sed "s/^.*[0-9].//g"
 
 
