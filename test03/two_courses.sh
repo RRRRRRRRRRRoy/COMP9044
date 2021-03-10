13b3ecc5f900b60fc0231295037ba5a60c4d3146
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-10 18:45:03 +1100

    q3

--- test03/two_courses.sh
+++ test03/two_courses.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cut -d'|' -f2 | sort | uniq -c  |sort -k1,1| egrep ' 2 [0-9]*' | tr -s " " | cut -d' ' -f3 | sort -k1,1
+cut -d'|' -f2,2 | sort | uniq -c  |sort -k1,1| egrep ' 2 [0-9]*' | tr -s " " | cut -d' ' -f3,3 | sort -k1,1
