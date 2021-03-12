b7fb503cb8c9eaa6ef37a5d985bb2d53ce5742f3
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 21:18:09 +1100

    test  Q5

--- lab04/create_music.sh
+++ lab04/create_music.sh
@@ -22,7 +22,7 @@ then
     do
         if test $counter -le 0
         then
-            new_album=$(echo "$line" | grep -e '.*style.*Triple.J.Hottest.100,.[0-9]{4}\|[0-9]{4}' |egrep -o 'Triple.J.Hottest.100,.[0-9]{4}')
+            new_album=$(echo "$line" | grep -e '.*style.*Triple J Hottest 100, [0-9]{4}\|[0-9]{4}' |egrep -o 'Triple J Hottest 100, [0-9]{4}')
             # How to check null
             # Source: https://stackoverflow.com/questions/20075811/what-does-the-operator-mean-in-a-shell-script
             # You can also write 
@@ -35,10 +35,10 @@ then
                 cd "$new_album"
             fi
         fi
-        
+
         if test $counter -gt 0
         then
-            song=$(echo "$line" | grep -e "^#.*")
+            song=$(echo "$line" | grep -e "^#")
             if ["$song" != ""]
             then
                 music_artist=$(echo "$line" | sed -r -e 's/\xE2\x80\x93.*?$//;s/\[\[[^]]*?\|([^]]*?)]]/\1/g;s/]//g;s/\[//g;s/^#//;s/^ *//;s/ *$//')
