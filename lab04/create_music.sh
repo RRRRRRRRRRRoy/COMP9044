6d9a590ab48a6142e6f6cace6f64206ee29b5714
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 20:20:11 +1100

    Test the dir exist or not in Q5

--- lab04/create_music.sh
+++ lab04/create_music.sh
@@ -1,1 +1,10 @@
 #!/bin/dash
+initial_path=$(pwd)
+initial_path=$(echo "$sample_path/$1")
+
+# How to check the dir exist or not
+# Source: https://www.cyberciti.biz/faq/howto-check-if-a-directory-exists-in-a-bash-shellscript/
+if [ ! -d "$2" ]
+then 
+    mkdir "$2"
+fi
