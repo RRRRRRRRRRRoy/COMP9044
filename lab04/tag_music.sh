ef21e832dc717775ddbbecd2e8638441cadcfbc4
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 19:15:28 +1100

    test Q4

--- lab04/tag_music.sh
+++ lab04/tag_music.sh
@@ -3,7 +3,7 @@
 
 for folder in "$@"
 do  
-    current_dir=$(pwd)
+    root_dir=$(pwd)
     cd "$folder"
     # How to use find and the meaning of type f
     # Source: https://explainshell.com/explain?cmd=find+folder+-type+f
@@ -12,18 +12,18 @@ do
         get_current_music_path=$(pwd)
         music_track=$(echo "$music" | cut -d' ' -f1,1)
         music_title=$(echo "$music" | sed -r 's/^..*?- (.+?) -..*?$/\1/')
-        music_artist=$(echo "$music" | sed 's/^..*- //;s/\.mp3//')
+        music_artist=$(echo "$music" | sed 's/^..*- //'| sed 's/\.mp3//')
         music_album=$(echo "$get_current_music_path" | sed 's/^..*\///')
         music_year=$(echo "$music_album" | cut -d',' -f2,2 | sed 's/^ //')
         # music_comment=""
 
-        id3 -t "$music_title" "$music" >&2
-        id3 -a "$music_artist" "$music" >&2
-        id3 -T "$music_track" "$music" >&2
-        id3 -A "$music_album" "$music" >&2
-        id3 -y "$music_year" "$music" >&2
+        id3 -t "$music_title" "$music" >/dev/null
+        id3 -a "$music_artist" "$music" >/dev/null
+        id3 -T "$music_track" "$music" >/dev/null
+        id3 -A "$music_album" "$music" >/dev/null
+        id3 -y "$music_year" "$music" >/dev/null
         # id3 -c "$music_comment" "$music" 
     done
     # After finished this folder back to the root and continue
-    cd $current_dir
+    cd $root_dir
 done
