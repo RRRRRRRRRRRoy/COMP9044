43702ae7123ede4896122d95c7ef1f68c9741424
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 16:35:10 +1100

    Q2 test

--- lab04/email_image.sh
+++ lab04/email_image.sh
@@ -6,14 +6,15 @@ for image in "$@"
 do
     echo "$image displayed to screen if possible"
 
-    echo "Address to e-mail this image to?"
+    echo -n "Address to e-mail this image to?"
     read email_address
 
-    echo "Message to accompany image?"
+    echo -n "Message to accompany image?"
     read message
 
     echo "$image sent to $email_address"
 
+    $get_name = $(echo $image| sed 's/[a-zA-Z0-9]*.png/[a-zA-Z0-9]*/g')
     
-    echo "$message" | mutt -e 'set copy=no' -a "$image" -- "$address"
+    echo "$message" | mutt -s "$get_name" -e 'set copy=no' -a "$image" -- "$address"
 done
