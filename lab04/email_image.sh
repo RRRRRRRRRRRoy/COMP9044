8b4b77ad9d2cdfe7b4e85b461ec88f148b1e6f0e
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 16:16:18 +1100

    Q2

--- lab04/email_image.sh
+++ lab04/email_image.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
 # exercise 02 for lab04
+
+for image in "$@"
+do
+    echo "$image displayed to screen if possible"
+
+    echo "Address to e-mail this image to?"
+    read email_address
+
+    echo "Message to accompany image?"
+    read message
+
+    echo "$image sent to $email_address"
+
+    echo 'Penguins are cool.' | mutt -s 'penguins!' -e 'set copy=no' -a penguins.png -- nobody@nowhere.com
+done
