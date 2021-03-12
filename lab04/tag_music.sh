ecc373f5f8d570f0747c521daa4402ebba405dad
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 19:08:49 +1100

    test Q4

--- lab04/tag_music.sh
+++ lab04/tag_music.sh
@@ -15,14 +15,14 @@ do
         music_artist=$(echo "$music" | sed 's/^.+- //;s/\.mp3//')
         music_album=$(echo "$get_current_music_path" | sed 's/^.+\///')
         music_year=$(echo "$music_album" | cut -d',' -f2 | sed 's/^ //')
-        music_comment=""
+        #music_comment=""
 
         id3 -t "$music_title" "$music" >/dev/null
         id3 -a "$music_artist" "$music" >/dev/null
         id3 -T "$music_track" "$music" >/dev/null
         id3 -A "$music_album" "$music" >/dev/null
         id3 -y "$music_year" "$music" >/dev/null
-        id3 -c "$music_comment" "$music" 
+        #id3 -c "$music_comment" "$music" 
     done
     # After finished this folder back to the root and continue
     cd $current_dir
