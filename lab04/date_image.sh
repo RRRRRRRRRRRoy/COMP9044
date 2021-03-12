0e628b911c5e9ec3107411e171cb01c29ade7836
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 17:08:22 +1100

    change the error of image

--- lab04/date_image.sh
+++ lab04/date_image.sh
@@ -4,5 +4,8 @@
 
 for image in "$@"
 do
+    get_dir_info=$(ls -l "$image"|tr -s " ")
+    get_time_stamp=$(echo "$get_dir_info"|cut -d" " -f6-8)
 
+    convert -gravity south -pointsize 36 -draw "text 0,10 "$time"" "$image" "$image"
 done
