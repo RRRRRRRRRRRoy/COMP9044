d4dc929de70cdcdcb9ca5779a55b1368e0e9b060
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 16:25:08 +1100

    Q2

--- lab04/email_image.sh
+++ lab04/email_image.sh
@@ -14,6 +14,6 @@ do
 
     echo "$image sent to $email_address"
 
-    get_mutt_s = sed 's/$image/[a-zA-Z0-9]*/g'
+    $get_mutt_s = sed 's/$image/[a-zA-Z0-9]*/g'
     echo '$message' | mutt -s '$get_mutt_s' -e 'set copy=no' -a "$img" -- "$address"
 done
