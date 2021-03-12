8f6385621cd452dd921a5328f8bbbf792ecb0655
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 21:58:07 +1100

    Q5

--- lab04/create_music.sh
+++ lab04/create_music.sh
@@ -38,9 +38,9 @@ then
             song=$(echo "$line" | egrep "^#")
             if ! test "$song" = ""
             then
-                music_artist=$(echo "$line" | sed -r -e 's/\xE2\x80\x93.*?$//'|sed -r -e 's/\[\[[^]]*?\|([^]]*?)]]/\1/g'| sed -r -e 's/]//g'| sed -r -e 's/\[//g' | sed -r -e 's/^#//' | sed -r -e's/^. *//' | sed -r -e 's/. *$//')
+                music_artist=$(echo "$line" | sed -r -e 's/\xE2\x80\x93.*?$//'|sed -r -e 's/\[\[[^]]*?\|([^]]*?)]]/\1/g'| sed -r -e 's/]//g'| sed -r -e 's/\[//g' | sed -r -e 's/^#//' | sed -r -e's/^ *//' | sed -r -e 's/ *$//')
                 
-                music_name=$(echo "$line" | sed -r -e 's/^.*?\xE2\x80\x93//' |sed -r -e 's/\[\[[^]]*?\|([^]]*?)]]/\1/g'| sed -r -e 's/]//g'| sed -r -e 's/\[//g'|sed -r -e 's#/#-#g' |sed -r -e 's/"//g' |sed -r -e 's/^. *//'| sed -r -e 's/. *$//')
+                music_name=$(echo "$line" | sed -r -e 's/^.*?\xE2\x80\x93//' |sed -r -e 's/\[\[[^]]*?\|([^]]*?)]]/\1/g'| sed -r -e 's/]//g'| sed -r -e 's/\[//g'|sed -r -e 's#/#-#g' |sed -r -e 's/"//g' |sed -r -e 's/^ *//'| sed -r -e 's/ *$//')
 
                 new_music_file="$counter - $music_name - $music_artist.mp3"
                 counter=$(( $counter + 1 ))
