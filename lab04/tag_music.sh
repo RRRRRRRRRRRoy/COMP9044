aca6ef8f1fb6fa6e97f1e8b831e6d624a5238f3e
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 19:07:59 +1100

    Q4 in lab4

--- lab04/tag_music.sh
+++ lab04/tag_music.sh
@@ -9,20 +9,20 @@ do
     # Source: https://explainshell.com/explain?cmd=find+folder+-type+f
     for music in *
     do
-        music_track=$(echo "$music" | cut -d' ' -f1,1)
-        music_title=$(echo "$music" | sed -r 's/^.*?- (.*?) -.*?$/\1/')
-        music_artist=$(echo "$music" | sed 's/^.*- //;s/\.mp3//')
         get_current_music_path=$(pwd)
-        music_album=$(echo "$get_current_music_path" | sed 's/^.*\///')
+        music_track=$(echo "$music" | cut -d' ' -f1,1)
+        music_title=$(echo "$music" | sed -r 's/^.+?- (.+?) -.+?$/\1/')
+        music_artist=$(echo "$music" | sed 's/^.+- //;s/\.mp3//')
+        music_album=$(echo "$get_current_music_path" | sed 's/^.+\///')
         music_year=$(echo "$music_album" | cut -d',' -f2 | sed 's/^ //')
-        #music_comment=""
+        music_comment=""
 
-        id3 -t "$music_title" "$music" 2>&1
-        id3 -a "$music_artist" "$music" 2>&1
-        id3 -T "$music_track" "$music" 2>&1
-        id3 -A "$music_album" "$music" 2>&1
-        id3 -y "$music_year" "$music" 2>&1
-        #id3 -c "$music_comment" "$music" 
+        id3 -t "$music_title" "$music" >/dev/null
+        id3 -a "$music_artist" "$music" >/dev/null
+        id3 -T "$music_track" "$music" >/dev/null
+        id3 -A "$music_album" "$music" >/dev/null
+        id3 -y "$music_year" "$music" >/dev/null
+        id3 -c "$music_comment" "$music" 
     done
     # After finished this folder back to the root and continue
     cd $current_dir
