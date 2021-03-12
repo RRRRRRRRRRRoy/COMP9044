ae674b2ca0e44e68f9552fba0ab4d2679d6cc017
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 18:46:58 +1100

    test Q4

--- lab04/tag_music.sh
+++ lab04/tag_music.sh
@@ -12,13 +12,13 @@ do
         music_artist=$(echo "$music" | sed 's/^.*-.//;s/\.mp3//')
         music_album=$(echo "$path" | sed 's/^.*\///')
         music_year=$(echo "$album" | cut -d',' -f2,2 | sed 's/^.//')
-        music_comment=""
+        #music_comment=""
 
         id3 -t "$music_title" "$music" 
         id3 -a "$music_artist" "$music" 
         id3 -T "$music_track" "$music" 
         id3 -A "$music_album" "$music" 
         id3 -y "$music_year" "$music" 
-        id3 -c "$music_comment" "$music" 
+        #id3 -c "$music_comment" "$music" 
     done
 done
