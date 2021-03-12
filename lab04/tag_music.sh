df2113b32d8461d6cc5495e4a1cfd1dd01b76199
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 18:13:00 +1100

    Q4

--- lab04/tag_music.sh
+++ lab04/tag_music.sh
@@ -6,5 +6,14 @@ do
     do
         music_title=$(id3 -l "current_music"|head 1|sed "s/.-./ /g"| cut -d" " -f2,2)
         music_track=$(id3 -l "current_music"|head 1|sed "s/.-./ /g"| cut -d" " -f1,1|cut -d'/' -f3,3)
+        music_artist=$(id3 -l "current_music"|head 1|sed "s/.-./ /g"| cut -d" " -f3,3 | sed 's/.mp3:$//g')
+        music_albym=$(id3 -l "current_music"|head 1|sed "s/.-./ /g"|cut -d" " -f1,1|cut -d'/' -f2,2)
+        year=$(echo $album | cut -d',' -f2,2 | sed 's/ //g')
+
+        command1=`id3 -t "$music_title" "$current_music"`
+        command2=`id3 -T "$music_track" "$current_music"`
+        command3=`id3 -a "$music_artist" "$current_music"`
+        command4=`id3 -A "$music_albym" "$current_music"`
+        command5=`id3 -y "$year" "$current_music"`
     done
 done
