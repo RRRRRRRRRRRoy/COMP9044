395b21f3ce850d4db2987c7550f7623fea0045be
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 19:29:06 +1100

    test Q4

--- lab04/tag_music.sh
+++ lab04/tag_music.sh
@@ -10,21 +10,24 @@ do
     for music in *
     do
         # Use the current path can get the info of album
-        get_current_music_path=$(pwd)
-        music_track=$(echo "$music" | cut -d' ' -f1,1)
-        music_title=$(echo "$music" | sed -r 's/^..*?- (.+?) -..*?$/\1/')
-        music_artist=$(echo "$music" | sed 's/^..*- //'| sed 's/\.mp3//')
-        music_album=$(echo "$get_current_music_path" | sed 's/^..*\///')
-        music_year=$(echo "$music_album" | cut -d',' -f2,2 | sed 's/^ //')
-        # music_comment=""
-
         # Each tag in id3
         # Source: https://tldp.org/HOWTO/MP3-HOWTO-13.html
-        id3 -t "$music_title" "$music" >/dev/null
-        id3 -a "$music_artist" "$music" >/dev/null
+        get_current_music_path=$(pwd)
+        music_track=$(echo "$music" | cut -d' ' -f1,1)
         id3 -T "$music_track" "$music" >/dev/null
+
+        music_artist=$(echo "$music" | sed 's/^..*- //'| sed 's/\.mp3//')
+        id3 -a "$music_artist" "$music" >/dev/null
+        
+        music_title=$(echo "$music" | sed -r 's/^..*?- (.+?) -..*?$/\1/')
+        id3 -t "$music_title" "$music" >/dev/null
+
+        music_album=$(echo "$get_current_music_path" | sed 's/^..*\///')
         id3 -A "$music_album" "$music" >/dev/null
+
+        music_year=$(echo "$music_album" | cut -d',' -f2,2 | sed 's/^ //')
         id3 -y "$music_year" "$music" >/dev/null
+        # music_comment="
         # id3 -c "$music_comment" "$music" 
     done
     # After finished this folder back to the root and continue
