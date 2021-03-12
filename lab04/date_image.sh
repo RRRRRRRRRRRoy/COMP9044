301ea9fcd0666e9c055e2b974ceb6ba48d1f4cb7
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 17:31:24 +1100

    Q3 finished

--- lab04/date_image.sh
+++ lab04/date_image.sh
@@ -2,10 +2,20 @@
 
 # exercise 3 date image in lab04
 
-for image in "$@"
-do
-    get_dir_info=$(ls -l "$image"|tr -s " ")
-    get_time_stamp=$(echo "$get_dir_info"|cut -d" " -f6-8)
+# for image in "$@"
+# do
+#     get_dir_info=$(ls -l "$image"|tr -s " ")
+#     get_time_stamp=$(echo "$get_dir_info"|cut -d" " -f6-8)
 
-    convert -gravity south -pointsize 36 -draw "text 0,10 "$time"" "$image" "$image"
+#     lastmodify=`stat "$img" | egrep "Modify" | sed "s/Modify: //g"| sed "s/[0-9]* [a-zA-Z]*/[a-zA-Z]* [0-9]*/"`
+#     convert -gravity south -pointsize 36 -draw "text 0,10 "$get_time_stamp"" "$image" "$image"
+# done
+
+for img in "$@"
+do
+    # using awk to re-arrange the sequence of the time stamp
+    text=`ls -l "$img" | tr -s ' ' | cut -d' ' -f6-8|awk '{print $2,$1,$3}'`
+    
+    convert -gravity south -pointsize 36 -draw "text 0,10 '$text'" "$img" "$img"
+    
 done
