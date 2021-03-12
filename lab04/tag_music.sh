ac1748576d423b2cc1201a9bd8baafaa32e072c9
“rrrrrrrrrrroy” <rrrrrrrrrrroy720@gmail.com>
2021-03-12 18:07:19 +1100

    Q4 tag music

--- lab04/tag_music.sh
+++ lab04/tag_music.sh
@@ -4,5 +4,7 @@ do
     # Source: https://explainshell.com/explain?cmd=find+folder+-type+f
     find "$folder" -type f | while read current_music
     do
+        music_title=$(id3 -l "current_music"|head 1|sed "s/.-./ /g"| cut -d" " -f2,2)
+        music_track=$(id3 -l "current_music"|head 1|sed "s/.-./ /g"| cut -d" " -f1,1|cut -d'/' -f3,3)
     done
 done
