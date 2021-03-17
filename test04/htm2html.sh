dbeb37a7deea61b9eb3815d5ce5bb5d255429d51
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-17 12:04:36 +1100

    test Q2

--- test04/htm2html.sh
+++ test04/htm2html.sh
@@ -4,7 +4,7 @@ ls | ls | egrep ".*\.htm$" | while read old_name
 do
     new_file_name=$(echo "$old_name" | sed "s/\.htm$/\.html/g")
     # test file exist
-    if [ ! -f "$new_file_name"]
+    if [ ! -f "$new_file_name" ]
     then 
         # not exist change the name
         mv "$old_name" "$new_file_name" 1>&2
