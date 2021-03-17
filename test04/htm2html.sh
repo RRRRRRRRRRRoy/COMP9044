d0cb41916f98e906945a12ab24618c2b222393c8
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-17 12:02:54 +1100

    changing the q2 and Q3 vlab test

--- test04/htm2html.sh
+++ test04/htm2html.sh
@@ -4,13 +4,13 @@ ls | ls | egrep ".*\.htm$" | while read old_name
 do
     new_file_name=$(echo "$old_name" | sed "s/\.htm$/\.html/g")
     # test file exist
-    if test -f "$new_file_name"
+    if [ ! -f "$new_file_name"]
     then 
+        # not exist change the name
+        mv "$old_name" "$new_file_name" 1>&2
+    else
         # if exist quit
         echo "$new_file_name exists" 1>&2
         exit 1
-    else
-        # not exist change the name
-        mv "$old_name" "$new_file_name" 1>&2
     fi
 done
--- test04/missing_include.sh
+++ test04/missing_include.sh
@@ -3,12 +3,15 @@
 for c in "$@"
 do 
     cat "$c" | egrep "^#include.\"" | cut -d'"' -f2,2| while read header
-    do
-        if test -f  "$header"
+    do  
+        # -d is for dir not for files 
+        if [ ! -f  "$header" ]
         then
-            continue
-        else
+            # not exist print error
             echo "$header" included into "$c" does not exist
+        else
+            # exist continue
+            continue
         fi
     done
 done
