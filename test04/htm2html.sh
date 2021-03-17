b41d98c0fb98d2b63e871872eaf0eedd07cae70a
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-17 11:48:44 +1100

    test Q2

--- test04/htm2html.sh
+++ test04/htm2html.sh
@@ -2,7 +2,7 @@
 
 ls | ls | egrep ".*\.htm$" | while read old_name
 do
-    new_file_name=$(echo "$old_name" | sed "s/\.htm$/\.html/g")
+    new_file_name=$(echo "$old_name" | sed "s/^.*\.htm$/\.html/g")
     # test file exist
     if test -f "$new_file_name"
     then 
