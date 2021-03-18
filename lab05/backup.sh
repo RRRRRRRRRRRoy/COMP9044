65f6a8998f44667095afd8a19d6031aa367c746c
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-19 10:56:02 +1100

    lab05 question1

--- lab05/backup.sh
+++ lab05/backup.sh
@@ -1,1 +1,11 @@
 #!/bin/dash
+
+ filename="$1"
+ counter=0
+ # get the file name
+ filename_cut=$(echo $filename | sed "s/.txt//g")
+ filename_copy=".$filename_cut.txt.$number"
+ contain=$(ls $filename_copy)
+ while [ 1 ]
+ do
+    if []
