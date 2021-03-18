7586efd5018d133c5d5e645d4defde47f215331f
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-19 10:59:14 +1100

    Q1 test

--- lab05/backup.sh
+++ lab05/backup.sh
@@ -8,4 +8,16 @@
  contain=$(ls $filename_copy)
  while [ 1 ]
  do
-    if []
+    if [ "$contain" == "" ]
+    then
+        :
+    else
+        counter=$(($counter+1))
+        filename_copy=".$filename_cut.txt.$number"
+        contain=$(ls $filename_copy)
+    done
+done 
+
+# copy files
+cp $filename $filename_copy
+echo "Backup of 'n.txt' saved as '.n.txt.3'"
