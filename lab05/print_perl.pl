a60b11fac0ef8cd897e9c2df0715d4db723e0b76
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-19 18:16:08 +1100

    a good start

--- lab05/print_perl.pl
+++ lab05/print_perl.pl
@@ -1,2 +1,8 @@
 #!/usr/bin/perl -w
 
+# This question is to write a perl program
+# THis program can run perl program
+# perl(perl(print))
+
+# The kind of the perl program
+print "#!/usr/bin/perl -w\n";
