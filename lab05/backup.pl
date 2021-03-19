b64d75415fd852b6ebf8fd3b53a555213b603f32
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-19 12:37:37 +1100

    check the file is exist or not

--- lab05/backup.pl
+++ lab05/backup.pl
@@ -4,7 +4,14 @@ $filename_input = $ARGV[0];
 #@filename_cut=();
 $counter = 0;
 @filename_cut=split('\.',$filename_input);
-$filename= $filename_cut[0]
+$filename= $filename_cut[0];
 # Test the filename is cut
 # print "$filename_cut[0]";
-$filename_copy=".$filename.txt.$counter"
+$filename_copy=".$filename.txt.$counter";
+
+# check whether the backup is exist
+# Source: https://alvinalexander.com/blog/post/perl/how-test-file-exists-perl/
+while (-e "$filename_copy"){
+    $counter = $counter + 1;
+    $filename_copy=".$filename.txt.$counter";
+}
