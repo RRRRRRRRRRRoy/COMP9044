51b82ebdfb58ac3d08513d6706de62d8618d5885
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-19 11:20:34 +1100

    Lab01 finished

--- lab05/backup.sh
+++ lab05/backup.sh
@@ -1,23 +1,21 @@
 #!/bin/dash
 
- filename="$1"
- counter=0
- # get the file name
- filename_cut=$(echo $filename | sed "s/.txt//g")
- filename_copy=".$filename_cut.txt.$number"
- contain=`ls $filename_copy`
- while [ 1 ]
- do
-    if [ "$contain" == "" ]
-    then
-        :
-    else
-        counter=$(($counter+1))
-        filename_copy=".$filename_cut.txt.$number"
-        contain=$(ls $filename_copy)
-    fi
+filename="$1"
+counter=0
+# get the file name
+filename_cut=$(echo $filename | sed "s/.txt//g")
+filename_copy=".$filename_cut.txt.$counter"
+contain=$(ls $filename_copy 2>/dev/null)
+while [ "$contain" != "" ]
+do
+
+    counter=$(($counter+1))
+    filename_copy=".$filename_cut.txt.$counter"
+    contain=$(ls $filename_copy 2>/dev/null)
+
 done 
 
 # copy files
 cp $filename $filename_copy
-echo "Backup of 'n.txt' saved as '.n.txt.3'"
+echo "Backup of '$filename' saved as '$filename_copy'"
+
