9d40856c5a73a88ce6a3606d4d47590595037b88
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-19 11:59:25 +1100

    lab 01 finish lab02 start

--- lab05/backup.sh
+++ lab05/backup.sh
@@ -4,15 +4,17 @@ filename="$1"
 counter=0
 # get the file name
 filename_cut=$(echo $filename | sed "s/.txt//g")
+# arrange the backup name
 filename_copy=".$filename_cut.txt.$counter"
 contain=$(ls $filename_copy 2>/dev/null)
 while [ "$contain" != "" ]
 do
-
+    # counter adding 1
     counter=$(($counter+1))
+    # adding number to the backup
     filename_copy=".$filename_cut.txt.$counter"
+    # avoiding /dev/null
     contain=$(ls $filename_copy 2>/dev/null)
-
 done 
 
 # copy files
