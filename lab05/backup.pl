6949365b7d4670cb127995eba3b80944e07419f8
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-19 12:35:00 +1100

    lab02

--- lab05/backup.pl
+++ lab05/backup.pl
@@ -1,1 +1,10 @@
 #!/bin/perl -w
+
+$filename_input = $ARGV[0];
+#@filename_cut=();
+$counter = 0;
+@filename_cut=split('\.',$filename_input);
+$filename= $filename_cut[0]
+# Test the filename is cut
+# print "$filename_cut[0]";
+$filename_copy=".$filename.txt.$counter"
