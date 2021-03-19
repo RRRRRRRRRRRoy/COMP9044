68bbbdbc24783342ff7b7b744f1038c1e57bbc1e
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-19 14:40:38 +1100

    test Q4 in lab05

--- lab05/snapshot.pl
+++ lab05/snapshot.pl
@@ -3,7 +3,7 @@
 # Getting the files in the current directory
 # Source: https://perldoc.perl.org/functions/glob
 @current_file = glob("*");
-condition=ARGV[0];
+condition=$ARGV[0];
 if (($condition eq "save") or ($condition eq "load")){
     # STEP 1 create the directory
     $counter = 0;
