3fa2a404340fc3e2885693a12689166206029b52
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-19 18:59:44 +1100

    check double quotes and backslashes

--- lab05/perl_print.pl
+++ lab05/perl_print.pl
@@ -5,7 +5,18 @@
 # perl(perl(print))
 
 # The kind of the perl program
+# How to understanding the meaning of =~
+# Source: https://stackoverflow.com/questions/10405868/what-does-mean-in-perl
 print "#!/usr/bin/perl -w\n";
 $input=$ARGV[0];
-
+if($input =~ /\"/){
+    $temp = $input;
+    $a =~ s/"/\\"/g;
+    print "print \"$a\\n\";";
+}
+if($input =~ /\\/){
+    $temp = $input;
+    $temp =! s/\\/\\\\/g;
+    print "print \"$temp\\n\";"
+}
 print "print \"$input\\n\";";
