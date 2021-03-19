6ec74ac5fd83e0b71cbbffe94a4d4ccdf308907b
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-19 13:25:16 +1100

    lab03 vlab test

--- lab05/snapshot-load.sh
+++ lab05/snapshot-load.sh
@@ -5,6 +5,47 @@ counter_bp=$1;
 
 ./snapshot-save.sh
 
-echo "Restoring snapshot $counter_bp";
+echo "Restoring snapshot $counter_bp"
 
+for file in *
+do  
+    # avoiding copy the same current file
+	if [ "$file" != "snapshot-save.sh" ] 
+    then 
+        # avoiding copy the same current file
+        if [ "$file" != "snapshot-load.sh" ]
+	    then
+            if [ "$file" != "backup.sh" ]
+            then
+                if [ "$file" != "backup.pl" ]
+                then
+                    if [ "$file" != "snapshot.pl" ]
+                    then
+			            rm $file
+                    else
+                        # pass
+                        :
+                    fi
+                else
+                    # pass
+                    :
+                fi
+            else
+                # pass
+                :
+            fi
+	    else
+            # pass
+            :
+        fi
+    else
+        # pass
+        :
+    fi
+done
 
+back_dir=".snapshot.$counter_bp"
+for backup_file in $back_dir/*
+do
+	cp $backup_file . 
+done
