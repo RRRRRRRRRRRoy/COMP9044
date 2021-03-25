942332bee105767f7f0e7559464d989f33cc1293
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-26 09:31:28 +1100

    Q3

--- test05/ls_identical.sh
+++ test05/ls_identical.sh
@@ -1,14 +1,15 @@
 #!/bin/dash
 
-directory1=$ARGV[1]
-directory2=$ARGV[2]
+directory1=$1
+directory2=$2
 
 for files in $directory1/*
 do
 	filename=`echo "$files"|sed 's|.*\/||g'`
 	if [ -e "$2/$filename" ] 
 	then
-		is_different=$(diff "$files" "$directory2/$filename"|wc -l)
+        get_difference=$(diff "$files" "$directory2/$filename")
+		is_different=$($get_difference|wc -l)
 		
         if [ "$is_different" -eq 0 ]
         then
