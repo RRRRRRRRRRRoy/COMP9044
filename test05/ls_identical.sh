63234da362bc844739e7aa471273fac605a8ea07
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-26 09:07:27 +1100

    test Q3

--- test05/ls_identical.sh
+++ test05/ls_identical.sh
@@ -6,7 +6,18 @@ directory2=$ARGV[2]
 for files in $directory1/*
 do
     filename=$(echo "$filename" | sed 's|.+\/||g'`) 
-    if [ -e "$directory2/$filename" ]
+    # if exist
+    if [ ! -e "$directory2/$filename" ]
     then
+        :
+    else
         is_different=$(diff "$files" "$directory2/$filename"|wc -l)
-        
+        # check the differences flag
+        if [ $is_different -eq 0 ]
+        then
+            echo "$filename"
+        else
+            :
+        fi
+    fi
+done
