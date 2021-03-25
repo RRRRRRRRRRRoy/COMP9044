5c6023fab6c17c22432ef80a499d68ab866ca008
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-26 09:34:29 +1100

    Q3

--- test05/ls_identical.sh
+++ test05/ls_identical.sh
@@ -5,7 +5,7 @@ directory2=$2
 
 for files in $directory1/*
 do
-	filename=`echo "$files"|sed 's/.*\///g'`
+	filename=`echo "$files"|sed 's/.+\///g'`
 	if [ -e "$2/$filename" ] 
 	then
 		check_difference=$(diff "$files" "$directory2/$filename"|wc -l)
