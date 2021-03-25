3e67bb9b424aebc44b4ac9550eac7535a790e24f
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-26 09:37:18 +1100

    Q3

--- test05/ls_identical.sh
+++ test05/ls_identical.sh
@@ -1,17 +1,21 @@
 #!/bin/dash
 
+# get the directory
 directory1=$1
 directory2=$2
 
+# looping the files in the directory
 for files in $directory1/*
 do
-	filename=`echo "$files"|sed 's/..*\///g'`
+	filename=$(echo "$files"|sed 's/..*\///g')
+    # check the second file is exists or not
 	if [ -e "$2/$filename" ] 
 	then
 		check_difference=$(diff "$files" "$directory2/$filename"|wc -l)
-		
+        # check the difference flag between 2 files
         if [ "$check_difference" -eq 0 ]
         then
+            # print the file name
             echo "$filename"
         else
 			:
