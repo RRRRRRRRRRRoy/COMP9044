d1b36c6389670e7353967315ef28475f96d2f6c9
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-26 20:27:31 +1100

    ass 1 init file

--- Ass1/girt-init.sh
+++ Ass1/girt-init.sh
@@ -5,4 +5,12 @@ if [ -d ".girt" ]
 then
     echo "girt-init: error: .girt already exist"
 	exit 1
+else 
+    :
 fi
+
+# if the directory is not exists
+# create the dir called .girt
+mkdir ".girt"
+
+echo "Initialized empty girt repository in .girt";
