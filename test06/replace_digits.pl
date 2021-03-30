c2aeb716dfd66e4ba8addacecc59605d94776454
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-30 12:43:46 +1100

    Q3 start

--- test06/replace_digits.pl
+++ test06/replace_digits.pl
@@ -1,1 +1,10 @@
 #!/usr/bin/perl -w
+
+$file=$ARGV[0]
+
+# copy from the question 2
+open my $stdin_stream,'<',$file or die "$!";
+
+
+# copy from the question 1
+open my $stdout_stream,'>',$file or die "$!";
