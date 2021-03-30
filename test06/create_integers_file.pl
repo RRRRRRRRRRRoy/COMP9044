00485282806ae4ff1619d6cca1cb7df8269aea05
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-30 12:27:19 +1100

    Q1

--- test06/create_integers_file.pl
+++ test06/create_integers_file.pl
@@ -5,11 +5,6 @@ if($#ARGV != 2){
     exit 1;
 }
 
-# check the second is larger than the first open
-if($ARGV[0] > $ARGV[1]){
-    exit 1; 
-}
-
 # start number
 $start_number = $ARGV[0];
 # end number
@@ -17,11 +12,13 @@ $end_number = $ARGV[1];
 # name
 $name = $ARGV[2];
 
-# file stream
-open $filestream,'>',$name or die $!;
-while($start_number <= $end_number){
-	print $filestream "$start_number\n";
-    $start_number=$start_number+1;
+if($start_number <= $end_number ){
+    # file stream
+    open $filestream,'>',$name or die $!;
+    while($start_number <= $end_number){
+	    print $filestream "$start_number\n";
+        $start_number=$start_number+1;
+    }
+    # Do not forget the close the file stream
+    close("$filestream");
 }
-
-close("$filestream");
