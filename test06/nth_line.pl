5b64c899af2886f97103f44c8e324582334760f5
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-30 12:33:31 +1100

    Q2 test in test 6

--- test06/nth_line.pl
+++ test06/nth_line.pl
@@ -1,1 +1,15 @@
 #!/usr/bin/perl -w
+$name = $ARGV[1];
+$nth_number = $ARGV[0];
+open $filestream,'>',$name or die $!;
+
+# using the array to read the data 
+@numbers = <$filestream>;
+
+# check the range of the data
+if($nth_number <= $#numbers+1){
+    print $numbers[$nth_number-1];
+}
+
+# clost the filestream
+close($filestream);
