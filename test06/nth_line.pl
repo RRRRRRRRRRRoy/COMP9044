b94c8cbe7f85e31335aaf842ad4df95b8389444b
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-03-30 12:39:20 +1100

    Q2 finidhed

--- test06/nth_line.pl
+++ test06/nth_line.pl
@@ -1,17 +1,18 @@
 #!/usr/bin/perl -w
 $name = $ARGV[1];
 $nth_number = $ARGV[0];
+# Copy from question1 in test6 do not forget to change the arrow
 open $filestream,'<',$name or die $!;
 
 # using the array to read the data 
 @numbers = <$filestream>;
 
+$length_array=$#numbers+1;
 # check the range of the data
-if($nth_number <= $#numbers+1){
-    print $numbers[$nth_number-1];
+if($nth_number <= $length_array){
+    $nindex = $nth_number-1;
+    print $numbers[$nindex];
 }
-
-
 
 # clost the filestream
 close($filestream);
