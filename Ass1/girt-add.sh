8361dd6550e991d9589ba169daac1c9f0ca1dbbb
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-03 10:08:09 +1100

    girt start

--- Ass1/girt-add.sh
+++ Ass1/girt-add.sh
@@ -1,1 +1,24 @@
 #!/bin/dash
+
+filelist=$@
+
+# check .girt directory is exist or not
+if [ ! -d .girt/ ]
+then
+fi
+
+# check the validation of the file name
+for file in $filelist
+do
+done
+
+# check the files are located in the directory
+for file in $filelist
+do
+done
+
+# copy the file to the branch
+# this is the girt-add operation
+for file in $filelist
+do
+done
