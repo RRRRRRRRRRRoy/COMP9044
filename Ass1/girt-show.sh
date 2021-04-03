5713da438360e1467b447f33ec2402023f7ca326
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-03 23:20:33 +1100

    checking the format of printing detail girt-show

--- Ass1/girt-show.sh
+++ Ass1/girt-show.sh
@@ -6,6 +6,22 @@
 if ! test -d .girt/
 then
     # Do not forget to change to girt-show function
-    echo "girt-show: error: no .girt directory containing girt repository exists";
+    echo "girt-show: error: no .girt directory containing girt repository exists"
     exit 1;
 fi
+
+# here is the printing details
+print_content_regex=$1;
+# using the grep to check whether it is in the good format
+match=$(echo $print_content_regex|grep -e '^[0-9]*:.+')
+if test "$match" != ""
+then
+    # matched not empty
+    :
+else
+    # matched is empty 
+    echo "girt-show: error: invalid object $print_content_regex"
+    exit 1
+fi
+
+
