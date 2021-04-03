f0d8dfb86ef3926193764de27b2051c21a7bfe9b
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-03 23:58:34 +1100

    subset 0 finished

--- Ass1/girt-show.sh
+++ Ass1/girt-show.sh
@@ -3,7 +3,7 @@
 
 # checking the .girt is exist or not
 # copy girt-commit.sh checking the .girt
-if ! test -d .girt/
+if [ ! -d .girt/ ]
 then
     # Do not forget to change to girt-show function
     echo "girt-show: error: no .girt directory containing girt repository exists"
@@ -23,7 +23,7 @@ fi
 
 #get the number before : and fileaname after :.
 printing_number=$(echo $print_content_slice|sed 's/:[a-zA-Z0-9].*$//g')
-printing_filename=$(echo $print_content_slice|sed 's/^[0-9].*://g')
+printing_filename=$(echo $print_content_slice|sed 's/^[0-9]*://g')
 
 # checking the printing number
 # echo "$printing_number"
@@ -40,12 +40,12 @@ if [ "$printing_number" != "" ]
 then
     # If the printing number is not empty
     # comparing the max number with the printing number
-    if test $printing_number -le $max_repository_number
+    if [ $printing_number -le $max_repository_number ]
     then
         # getting the files in the dir and raising the exception
         files_in_directory=$(ls .girt/repository/$printing_number/$printing_filename 2>/dev/null)
         # files_in_directory is empty
-        if test "$files_in_directory" = ""
+        if [ "$files_in_directory" = "" ]
         then
             # empty ----> not include the file
             echo "girt-show: error: '$printing_filename' not found in commit $printing_number"
@@ -60,5 +60,11 @@ then
     fi
 else
     # If the printing number is empty
-    
+    files_in_directory=$(ls .girt/branch/$current_branch/index/$printing_filename 2>/dev/null)
+    if [ "$files_in_directory" != "" ]
+    then
+        cat ".girt/branch/$current_branch/index/$printing_filename";
+    else
+        echo "girt-show: error: '$printing_filename' not found in index";
+    fi
 fi
