c634ac720d97ee9038603e05a78d386dd4e4ef7e
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-03 18:31:10 +1100

    Commit check file modification

--- Ass1/girt-commit.sh
+++ Ass1/girt-commit.sh
@@ -28,7 +28,19 @@ changes_list=$(ls -c .girt/repository|wc -l)
 changes_number=$(echo $changes_list | sed 's/ //g')
 if changes_number -ne 0
 then
-
+    # sorting and getting latest braches
+    branch_list=$(ls .girt/branch/$current_branch/repository|sort)
+    latest_modify_branch=$(echo $branch_list|tail -n 1)
+    #need to check if index and latest repo contain same number of files.
+    file_index_number=$(ls .girt/branch/$current_branch/index/|wc -l)
+    #repo_file_c=`ls .girt/repo/$latest|wc -l`;
+    repository_branch_number=$(ls .girt/branch/$current_branch/repository/$latest_modify_branch|wc -l)
+    if [ "$file_index_number" -eq "$repository_branch_number" ]
+    then
+        :
+    else
+        changes_counter=1
+    fi
 else
     changes_counter=1;
 
