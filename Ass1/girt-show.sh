68773b55f51091f60c1ce02ad5c03432b5cd3113
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-03 23:00:31 +1100

    girt-show start

--- Ass1/girt-show.sh
+++ Ass1/girt-show.sh
@@ -1,1 +1,11 @@
 #!/bin/dash
+#!/usr/bin/env dash
+
+# checking the .girt is exist or not
+# copy girt-commit.sh checking the .girt
+if ! test -d .girt/
+then
+    # Do not forget to change to girt-show function
+    echo "girt-show: error: no .girt directory containing girt repository exists";
+    exit 1;
+fi
