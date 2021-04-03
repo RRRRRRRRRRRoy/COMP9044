3a813084b13f1f74769cffd6261061e30aaa0692
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-03 15:44:18 +1100

    check validation need test

--- Ass1/girt-add.sh
+++ Ass1/girt-add.sh
@@ -5,11 +5,19 @@ filelist=$@
 # check .girt directory is exist or not
 if [ ! -d .girt/ ]
 then
+    echo "girt-add: error" 
+    exit 1;
 fi
 
 # check the validation of the file name
 for file in $filelist
 do
+    match_regex=$(echo "$file" | egrep "^[0-9a-zA-Z][0-9a-zA-Z\.\-]+*$")
+    if [ "" == "$match_regex" ]
+    then
+        echo "shrug-add: error:"
+        exit 1;
+    fi
 done
 
 # check the files are located in the directory
