6e087a7737a99e24f9d2e84bc686bd4504419244
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-05 09:20:28 +1000

    orca.pl

--- lab07/orca.pl
+++ lab07/orca.pl
@@ -1,25 +1,19 @@
-#!/usr/bin/perl -w
+# #!/usr/bin/perl -w
 
+# getting the filename from the input
 $filename=$ARGV[0];
 
+# setting counter
 $sum=0;
 
-open my $stdin,'<',"$filename" or die "$!";
+# setting filestream to read file from the filename
+# Source: http://perltraining.com.au/tips/2005-11-17.html
+open (my $stdin,'<',"$filename") or die "$!";
 
+# list to store the data of input stream
 @whale_records=();
 while($line = <$stdin>){
+    # push operation
+    # Source: https://www.geeksforgeeks.org/perl-push-function/
     push @whale_records, $line;
 }
-
-foreach $record (@whale_records){
-    # print($record);
-    $record_name=substr($record,12);
-    # print($record_name);
-    if ("$record_name" eq "Orcas"){
-        @info = split / /, $record;
-        # print $info[1];
-        $sum+=$info[1];
-    }
-}
-
-print "$sum Orcas reported in whales.txt";
