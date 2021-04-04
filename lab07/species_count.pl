1174f9f0c29ca94570adfbdfab5e4d8aab070240
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-05 09:35:41 +1000

    change the the pod to line and adding + in split

--- lab07/species_count.pl
+++ lab07/species_count.pl
@@ -22,10 +22,11 @@ $while_type=$ARGV[0];
 $pods=0;
 $individual_oberservation=0;
 foreach $line (@whale_list){
-    if($line =~ m/$while_type/){
+    if($line =~ m"$while_type"){
         $pods = $pods + 1;
-        @info = split / /, $pod;
-        $individual_oberservation = $info[0] + $individual_oberservation;
+        @info = split / +/, $line;
+        # print "@info";
+        $individual_oberservation = $info[1] + $individual_oberservation;
     }
 }
 print "$while_type observations: $pods pods, $individual_oberservation individuals\n";
