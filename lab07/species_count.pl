83793e19a69e89c5220d4dbce6bd6b310259935b
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-05 09:30:50 +1000

    lab07 species_count start

---
+++ lab07/species_count.pl
@@ -0,0 +1,26 @@
+# #!/usr/bin/perl -w
+
+# getting the filename from the input
+$filename=$ARGV[1];
+
+# setting counter
+$sum=0;
+
+# setting filestream to read file from the filename
+# Source: http://perltraining.com.au/tips/2005-11-17.html
+open (my $stdin,'<',"$filename") or die "$!";
+
+# list to store the data of input stream
+@whale_list=();
+while($line = <$stdin>){
+    # push operation of array
+    # Source: https://www.geeksforgeeks.org/perl-push-function/
+    push @whale_list, $line;
+}
+
+$while_type=$ARGV[0];
+$pods=0;
+$individual_oberservation=0;
+foreach $line (@whale_list){
+    :
+}
