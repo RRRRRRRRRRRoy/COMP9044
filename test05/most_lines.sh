d2bc10f4d700ba2ae67e86e4eefe7389c8f90fdd
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-06 09:43:12 +1000

    Q2 finish in lab05

--- test05/most_lines.sh
+++ test05/most_lines.sh
@@ -3,17 +3,21 @@
 max=0
 max_name=""
 
+# setting counter to doing the compare
 for file in "$@"
 do
     number=$(awk '{print NR}' $file|tail -n1)
+    # This is the test point
     # echo "current_number_line: $number"
     name="$file"
+    # This is the test point
     # echo "current_file_name: $name"
     if [ $max -lt $number ]
     then
         max=$number
         max_name=$name
     fi
+    # This is the test point
     # echo "Max: $max"
     # echo "Max_name: $max_name"
 done
