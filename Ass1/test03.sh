9583fad200dbac23e7dbc11c9251e5a840c07ed7
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-05 20:48:25 +1000

    test03

--- Ass1/test03.sh
+++ Ass1/test03.sh
@@ -2,8 +2,20 @@
 
 sh ./girt-init
 sh ./girt-checkout b1
-echo COMP9044 looks Cool > good.txt
-sh ./girt-add good.txt
-sh ./girt-commit -m "commit-a"
-sh ./girt-branch b1
-sh ./girt-checkout b1
+echo COMP9044 looks Cool > a
+echo perl is the best lang > b
+sh ./girt-commit -m "commit-0"
+sh ./girt-add a b
+sh ./girt-commit -m "commit-1"
+seq 1 23 > a
+sh ./girt-commit -a -m "commit-2"
+echo Shell is better >> b
+sh ./girt-add b
+sh ./girt-status
+sh ./girt-commit -m "commit-3"
+sh ./girt-show 0:b
+sh ./girt-show 0:a
+sh ./girt-show 1:a
+sh ./girt-rm a
+sh ./girt-rm b
+sh ./girt-status
