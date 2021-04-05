79460c2e3bf86e232e7ad8ea18684ea95833e610
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-05 11:34:03 +1000

    whale_summary test

--- lab07/whale_summary.pl
+++ lab07/whale_summary.pl
@@ -2,19 +2,6 @@
 
 # getting the filename from the input
 $filename=$ARGV[0];
-
-# setting filestream to read file from the filename
-# Source: http://perltraining.com.au/tips/2005-11-17.html
-open $stdin,"<","$filename" or die "$!";
-
-# whale_data to store the data of input stream
-@whale_data=();
-while($line = <$stdin>){
-    # push operation of array
-    # Source: https://www.geeksforgeeks.org/perl-push-function/
-    push @whale_data, $line;
-}
-
 # setting the array for name storage
 @name_array=();
 # setting the hash also dictionary to store the data of each name1
@@ -25,6 +12,20 @@ while($line = <$stdin>){
 # counting the number of individuals key is whale name
 %whale_individual=();
 
+
+# setting filestream to read file from the filename
+# Source: http://perltraining.com.au/tips/2005-11-17.html
+open ($stdin,"<","$filename") or die "$!";
+
+# whale_data to store the data of input stream
+@whale_data=();
+while($line = <$stdin>){
+    # push operation of array
+    # Source: https://www.geeksforgeeks.org/perl-push-function/
+    push @whale_data, $line;
+}
+
+
 foreach $line (@whale_data){
     # cutting the from space
     $line =~ s/^\s+//;
@@ -34,29 +35,45 @@ foreach $line (@whale_data){
     @info = split / +/, $line;
     # getting the individual number
     $individual_oberservation = $info[1];
-    $get_whale_name = @info[2];
+    $whale_name = @info[2];
+    # changing to lower case
+    # Source: https://perldoc.perl.org/functions/lc
+    $whale_name = lc($whale_name);
     # This is to remove the s and get the type name of whale
-    $get_whale_name =~ s/s$//;
+    $whale_name =~ s/s$//;
     # how to use chomp
     # Source: https://perldoc.perl.org/functions/chomp
-    chomp $get_whale_name;
+    # chomp $whale_name;
 
     # exists is for checking the array or hash exists or not
     # Source: https://perldoc.perl.org/functions/exists
-    if(exists($whale_pods{$get_whale_name})){
-        $whale_pods{$get_whale_name} = $whale_pods{$get_whale_name} + 1;
-        $whale_individual{$get_whale_name} = $whale_individual{$get_whale_name} + $individual_oberservation;
-    }else{ 
-        # new whale
-        # print "Has not been observed before\n";
-        push @name_array, $get_whale_name;
-        $whale_pods{$get_whale_name} = 1;
-        $whale_individual{$get_whale_name} = $individual_oberservation;
+    # checking whale_pods is exist or not
+    if( ! exists($whale_pods{$whale_name})){
+        # checking whale_individual is exist or not
+        if ( ! exists($whale_individual{$whale_name})){
+            # new whale
+            # print "Has not been observed before\n";
+            # create a new tag
+            push @name_array, $whale_name;
+            # setting the new tag value as 1
+            $whale_pods{$whale_name} = 1;
+            # setting the invidual as the first individual_oberservation value
+            $whale_individual{$whale_name} = $individual_oberservation;
+        }
+    }else{
+        # pods in increasing 1
+        $whale_pods{$whale_name} = $whale_pods{$whale_name} + 1;
+        # adding the the current individual_oberservation value
+        $whale_individual{$whale_name} = $whale_individual{$whale_name} + $individual_oberservation;
+         
     }
 }
 
 # output the data in alphabetical order
-@name_array = sort(@name_array);
-foreach $get_whale_name(@name_array){
-    print "$get_whale_name observations: $whale_pods{$get_whale_name} pods, $whale_individual{$get_whale_name} individuals\n";
+# @name_array = sort(@name_array);
+# How to use sort to sort array
+# Source: https://perldoc.perl.org/functions/sort
+foreach $whale_name(sort(@name_array)){
+    # printing the result
+    print "$whale_name observations: $whale_pods{$whale_name} pods, $whale_individual{$whale_name} individuals\n";
 }
