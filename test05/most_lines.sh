758d91a142493b39917529b46b29791b34ab3f8a
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-06 09:30:37 +1000

    test 5 Q2

--- test05/most_lines.sh
+++ test05/most_lines.sh
@@ -1,5 +1,18 @@
 #!/bin/dash
-for file in $@
+
+max=0
+max_name=""
+
+for file in "$@"
 do
-    awk 'END{print max(NR) "," FILENAME}' $file | sort -n -k1,1   
+    current_number_line=$(awk '{print NR}' $file|tail -n1)
+    current_file_name="$file"
+    if [ $max -lt $current_number_line ]
+    then
+        max=$current_number_line
+        max_name=$curremt_file_name
+    fi
 done
+
+echo "$current_file_name"
+
