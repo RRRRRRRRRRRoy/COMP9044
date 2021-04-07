8973a9c0a7aaee231c34c1a3694e8b82b7c8a4dc
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-07 20:36:33 +1000

    Q5

--- lab08/identify_artist.pl
+++ lab08/identify_artist.pl
@@ -1,27 +1,37 @@
 #!/usr/bin/perl -w
 
-$keyword = $ARGV[0];
-
-# filelist
 @filelist = glob "lyrics/*.txt";
-
-
-foreach my $file (@filelist){
+# build my artist hash
+foreach $file (@filelist) 
+{
+    # get artist name
+    # String matching is from the following link
+    # Source: https://www.tutorialspoint.com/perl/perl_regular_expressions.htm
+    # Cutting the string
+    # Source: http://web.eecs.utk.edu/~bvanderz/cs460/notes/perl/perl2.html
+    # These part of code is copied from Q3 log_probability
     $artist = $file;
-    # delete lyrics/
-	$artist =~ s/lyrics\///g;
-    # Delete the .txt
-	$artist =~ s/\.txt//g;
-    # replace the _ by space
-	$artist =~ s/_/ /g;
+    $artist =~ s/lyrics\///;
+    $artist =~ s/\.txt//;
+    $artist =~ tr/_/ /;
 
-    # # setting filestream to read file from the filename
-    # # Source: http://perltraining.com.au/tips/2005-11-17.html
-    open (my $stdin,'<',"$file") or die "$!";
+    # read lyrics from every artist
+    open (my $stdin, '<', "$file") or die "$!";
     
-
-    # Do not forget to close the file stream
+    foreach $line (<$stdin>)
+    {
+        # This part of code is copied from Question1
+        # \w including char number also _
+        # Source: https://www.w3schools.com/jsref/jsref_regexp_wordchar.asp
+        $regrex = '[a-zA-Z]+';
+        @words = $line =~ /$regrex/g;
+        foreach $word (@words)
+        {
+            # The idea of this part of code is copied from Question2
+            $key =~ lc($word);
+            # doing the statistics for each artist
+            $artist_hash{"$artist"}{"$key"} = $artist_hash{"$artist"}{"$key"} + 1;
+        }
+    }
     close $stdin;
 }
-
-
