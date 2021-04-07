2246eeef29605339f9783e81f603fdab382d8b60
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-07 22:39:04 +1000

    testQ2

--- lab08/count_word.pl
+++ lab08/count_word.pl
@@ -1,17 +1,17 @@
 #!/usr/bin/perl -w
 
 # getting the filename from the input
-$seleted_word=$ARGV[0];
-$filename = $ARGV[1];
+#$seleted_word=$ARGV[0];
+#$filename = $ARGV[1];
 # # setting counter
 # # setting filestream to read file from the filename
 # # Source: http://perltraining.com.au/tips/2005-11-17.html
-open (my $stdin,'<',"$filename") or die "$!";
+#open (my $stdin,'<',"$filename") or die "$!";
 
 %words_dict=();
 
 # This is similar to Nvdia 2019 interview question counting words
-foreach $line(<$stdin>)
+foreach $line(<STDIN>)
 {
     foreach $word ($line =~ /([a-zA-Z]+)/g)
     {   
