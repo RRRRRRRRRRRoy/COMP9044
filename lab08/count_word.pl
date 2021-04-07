3d317aec81314284f706895dbcbcc99a66a1ed82
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-07 22:41:46 +1000

    Q2

--- lab08/count_word.pl
+++ lab08/count_word.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 
 # getting the filename from the input
-$seleted_word=$ARGV[0];
+my $seleted_word=$ARGV[0];
 #$filename = $ARGV[1];
 # # setting counter
 # # setting filestream to read file from the filename
@@ -13,7 +13,7 @@ $seleted_word=$ARGV[0];
 # This is similar to Nvdia 2019 interview question counting words
 foreach $line(<STDIN>)
 {
-    foreach $word ($line =~ /([a-zA-Z]+)/g)
+    foreach $word ($line =~ /[a-zA-Z]+/g)
     {   
         # using the dictionary to stroe the number of appearance
         $key = lc($word);
