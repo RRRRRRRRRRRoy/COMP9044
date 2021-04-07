7989ec90e09d7e11d2270e31cd8948c10b84157f
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-07 14:36:06 +1000

    Q1 lab08

--- lab08/total_words.pl
+++ lab08/total_words.pl
@@ -1,3 +1,16 @@
 #!/usr/bin/perl -w
 
 $counter = 0;
+
+# getting the filename from the input
+$filename=$ARGV[1];
+
+# setting filestream to read file from the filename
+# Source: http://perltraining.com.au/tips/2005-11-17.html
+open (my $stdin,'<',"$filename") or die "$!";
+
+foreach $line(<$stdin>){
+    $counter += $line =~ /[a-zA-Z]/g;
+}
+
+print "$total\n"
