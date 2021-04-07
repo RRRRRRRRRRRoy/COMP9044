1018a0a1d27ae7f13c86f4379606ed0d000e117f
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-07 16:36:58 +1000

    Q3 finish in lab08

---
+++ lab08/count_word.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+
+# getting the filename from the input
+$seleted_word=$ARGV[0];
+$filename = $ARGV[1];
+# # setting counter
+# # setting filestream to read file from the filename
+# # Source: http://perltraining.com.au/tips/2005-11-17.html
+open (my $stdin,'<',"$filename") or die "$!";
+
+%words_dict=();
+
+# This is similar to Nvdia 2019 interview question counting words
+foreach $line(<$stdin>)
+{
+    foreach $word ($line =~ /([a-zA-Z]+)/g)
+    {   
+        # using the dictionary to stroe the number of appearance
+        $key = lc($word);
+        $words_dict{$key}++;
+    }
+}
+
+print "$seleted_word occurred $words_dict{$seleted_word} times"
---
+++ lab08/frequency.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl -w
+
+
+$keyword = $ARGV[0];
+
+# filelist
+@filelist = glob "lyrics/*.txt";
+
+
+for $file (@filelist){
+	open (my $stdin,'<',"$file") or die "$!";
+
+    # String matching is from the following link
+    # Source: https://www.tutorialspoint.com/perl/perl_regular_expressions.htm
+    # Cutting the string
+    # Ig using
+    # Source: http://web.eecs.utk.edu/~bvanderz/cs460/notes/perl/perl2.html
+    # print $file;
+    # delete lyrics/
+	$file =~ s/lyrics\///g;
+    # Delete the .txt
+	$file =~ s/\.txt//g;
+    # replace the _ by space
+	$file =~ s/_/ /g;
+	$artist = $file;
+	$totalwords = 0;
+    # Counting the number of each word in the lyrics
+    %words_dict=();
+	while($line = <$stdin>){
+        # THis part of code is from Question1
+        $regrex = '[a-zA-Z]+';
+        @words = $line =~ /$regrex/g;
+        # Store the data in to a list using scalar counting the list
+        # Source: https://www.tutorialspoint.com/perl/perl_scalar.htm
+        $totalwords += scalar @words;
+        # This part of code is from question 2
+		for $word (@words){
+            # to lower cases
+			$key = lc($word);
+            $words_dict{$key}++;
+		}
+	}
+    # format printing
+    # Source: https://perldoc.perl.org/functions/printf
+	printf "%4d/%6d = %.9f %s\n", $words_dict{$keyword}, $totalwords, $words_dict{$keyword}/$totalwords,$artist;
+}
