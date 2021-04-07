4baf43892e5db4c1e833701f39c7cc89ec609f08
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-07 22:33:40 +1000

    Q1

--- lab08/total_words.pl
+++ lab08/total_words.pl
@@ -2,14 +2,14 @@
 
 
 # getting the filename from the input
-#$filename=$ARGV[0];
+$filename=$ARGV[0];
 
 # # setting counter
-#$sum=0;
+$sum=0;
 
 # # setting filestream to read file from the filename
 # # Source: http://perltraining.com.au/tips/2005-11-17.html
-#open $stdin,'<',$filename or die "$!";
+open $stdin,'<',$filename or die "$!";
 
 foreach $line(<STDIN>){
     # Using [a-zA-Z] only
