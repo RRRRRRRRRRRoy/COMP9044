0e619597a2f073c21c6b7aa0ac8ba650cbf54e66
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-08 11:22:48 +1000

    test Q2

--- test07/snap_n.pl
+++ test07/snap_n.pl
@@ -6,14 +6,14 @@ $input_string=$ARGV[0];
 # Setting the counter to counter the appearance
 $counter=0;
 
-%strings=();
 # setting filestream to read file from the filename
 # Source: http://perltraining.com.au/tips/2005-11-17.html
 # open (my $stdin,'<',"@ARGV") or die "$!";
 
 foreach $string(<STDIN>){
     $strings{$string} ++;
-    if($input_string eq $strings{$string}){
+    $current_string = $strings{$string};
+    if($input_string eq $current_string){
         # current appearance is 1
         if($counter != 0){
 		    next;
@@ -24,8 +24,6 @@ foreach $string(<STDIN>){
     }
 }
 
-if(! $output){
-	next;
-}else{
+if($output){
     print "Snap: $output";
 }
