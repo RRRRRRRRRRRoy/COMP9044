a39ad3f8e76a1fbe2fb7653d284b2e979075f69c
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-11 18:27:51 +1000

    checking the content in Q1

--- test08/middle_lines.pl
+++ test08/middle_lines.pl
@@ -7,13 +7,35 @@ $input_file=$ARGV[0];
 
 # Copy from question1 in test6 do not forget to change the arrow
 # Source: https://perldoc.perl.org/functions/open
-open ($filestream,'<',$input_file) or die $!;
+open ($fileinputstream,'<',$input_file) or die $!;
 
-foreach $line($filestream){
+foreach $line($fileinputstream){
     # chomp is to delete the empty line
     chomp $line;
     push @lines_list,$line;
 }
 
+# Getting the length of the list
 $length_of_file=@lines_list;
 $number_of_lines=$length_of_file;
+ 
+# checking odd
+$checking_odd=$number_of_lines % 2;
+# if the file is not empty
+if($number_of_lines > 0){
+    # The current line number is odd number
+    if($checking_odd == 1){
+        $middle_index = $number_of_lines/2
+        print "$lines_list[$middle_index]\n";
+    }
+    # The current line number is even number
+    if($checking_odd == 0){
+        # middle - 1 content ----> previous content
+        $middle_index = $number_of_lines/2
+        $previous_content = $lines_list[$middle_index-1];
+        # middle content ----> later content
+        $later_content = $lines_list[$middle_index];
+        print "$previous_content\n";
+        print "$later_content\n";
+    }
+}
