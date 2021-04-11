4495f7af9a4c29962b5971d9329ba9c4ede418ee
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-11 18:51:10 +1000

    Q3

--- test08/sort_file_lines.pl
+++ test08/sort_file_lines.pl
@@ -4,7 +4,7 @@
 $input_file=$ARGV[0];
 
 # createing a words list
-@words_list=();
+%words_dict=();
 
 # Copy from question1 in test6 do not forget to change the arrow
 # Source: https://perldoc.perl.org/functions/open
@@ -14,5 +14,19 @@ foreach $line($fileinputstream){
     # chomp is to delete the empty line
     # Source: https://perldoc.perl.org/functions/chomp
     chomp $line;
-    push @lines_list,$line;
+    $words_dict{$line} = length($line);
+}
+
+# Getting the key
+# Source: https://perldoc.perl.org/functions/keys
+@words_dict_key = keys %words_dict;
+
+# How to use <=> 
+# Source: https://stackoverflow.com/questions/7855815/meaning-of-less-than-equal-greater-than-in-perl
+# How to use cmp
+# Source: https://www.geeksforgeeks.org/perl-cmp-operator/
+@sorted_words_key = sort {$words_dict{$key1} <=> $words_dict{$key2} or $key1 cmp $key2} @words_dict_key;
+
+foreach $line (@sorted_words_key){
+	print "$line\n";
 }
