96306822ff87caea19408eb6e4f856aa8c4c9b4d
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-12 11:18:41 +1000

    Q2

--- lab08/count_word.pl
+++ lab08/count_word.pl
@@ -6,14 +6,14 @@ $counter = 0;
 $selected_word = $ARGV[0];
 
 while($line = <STDIN>){
-    $reprex = '[^a-z]+';
+    $regrex_word = '[^a-z]+';
     # based on the regrex cutting the word store in array
-	@words = split(/$reprex/i,$line);
+	@words = split(/$regrex_word/i,$line);
 
 	foreach $word (@words){
         # checking whether the word is same or not
         # Do not forget to change all these variables to lower case
-		if (lc($selected_word) eq lc($word)){
+		if (lc($selected_word) eq lc($word) and uc($selected_word) eq uc($word)){
             # Counter adding1
 			$counter += 1;
 		}
