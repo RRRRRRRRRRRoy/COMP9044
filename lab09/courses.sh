4822f9c4d418a877c8fe8e0e976b18e1b086c6f4
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-13 18:28:32 +1000

    Q1 in lab09

--- lab09/courses.sh
+++ lab09/courses.sh
@@ -1,10 +1,10 @@
 #!/bin/sh 
 
-
-# Source: https://cgi.cse.unsw.edu.au/~cs2041/21T1/lab/09/questions
-UNSW_COURSETABLE="http://timetable.unsw.edu.au/2020/$1KENS.html"
-
 major=$1
+# Source: https://cgi.cse.unsw.edu.au/~cs2041/21T1/lab/09/questions
+UNSW_COURSETABLE="http://timetable.unsw.edu.au/2020/"$major"KENS.html"
+
+
 # This part of code is modified from the the following links
 # SOurce: https://cgi.cse.unsw.edu.au/~cs2041/21T1/lab/09/questions
 # curl --location --silent http://www.timetable.unsw.edu.au/current/COMPKENS.html|head
@@ -13,13 +13,18 @@ major=$1
 # Source: https://unix.stackexchange.com/questions/196549/hide-curl-output
 curl --location --silent $UNSW_COURSETABLE | sort | uniq | while read line
 do
+    # Inverse matching Selected lines are those not matching any of the specified patterns.
+    # Source: https://www.unix.com/unix-for-dummies-questions-and-answers/115385-grep-v-option.html
     match_string=$(echo $line|egrep "^ *<td class=\"data\">.*\"$major[\d+]*.*\">.*</a></td>"|egrep -v ".*$major[\d+]*.*$major[\d+]*")
-    echo $match_string
-    if $match_string == ""
+    # checking the brief structure of the online data
+    # echo $match_string
+    if [ "$match_string" == "" ]
     then
-        # no matches have been found
         :
     else
         # finding matches cutting the data
+        course_info=$(echo $match_string|sed "s/.*\($major[0-9][0-9][0-9][0-9]*\)\.html[^>]*> *\([^<]*\).*/\1 \2/")
+		echo $course_info
     fi
 done
+
