3b4e2ff25958552542a130107aa40ff5b2ff268e
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-15 19:46:16 +1000

    Q2 lab09

--- lab09/courses.pl
+++ lab09/courses.pl
@@ -1,7 +1,12 @@
 #!/usr/bin/perl -w
 
-# $major=$ARGV[0];
+$major=$ARGV[0];
 
+my @match_result;
+
+# How to get the value in the array with uniq
+# Source: https://perlmaven.com/unique-values-in-an-array-in-perl
+use List::MoreUtils qw(uniq);
 # Testing this function
 # These part of code is from the website of COMP9044
 # Source: https://cgi.cse.unsw.edu.au/~cs2041/21T1/lab/09/questions
@@ -10,8 +15,21 @@
 use LWP::Simple;
 $UNSW_COURSETABLE = "http://www.timetable.unsw.edu.au/current/$ARGV[0]KENS.html";
 $web_source = get($UNSW_COURSETABLE) or die "Unable to get $UNSW_COURSETABLE";
-print "$web_source";
 
-@lines_in_web = split(/\n/, $web_source);
+# Testing whether getting the web source successfully
+# print "$web_source";
 
-print "$lines_in_web[1]";
+# cutting each line of the websource and testing
+@lines_in_web = split /\n/, $web_source;
+# print "$lines_in_web[1]";
+
+foreach $line (@lines_in_web){
+    # testing the current data reading is successful
+    # print "$line";
+    # print $line;
+	if($line =~ /^.*$major[0-9][0-9][0-9][0-9]*.*">.*<\/a>/){
+		if($line =~ /.*$major[0-9][0-9][0-9][0-9].*$major[0-9][0-9][0-9][0-9]/){
+			#print "$line";
+			next;
+		}
+
