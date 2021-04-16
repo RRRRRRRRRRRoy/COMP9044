0fb06e3b51835aa021ae658effa92dcc66a40473
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-16 12:05:53 +1000

    Q1 test

--- lab09/courses.sh
+++ lab09/courses.sh
@@ -11,11 +11,11 @@ UNSW_COURSETABLE="http://timetable.unsw.edu.au/current/"$major"KENS.html"
 # adding the sort and uniq to clean the online data
 # How to hide curl output
 # Source: https://unix.stackexchange.com/questions/196549/hide-curl-output
+# Inverse matching Selected lines are those not matching any of the specified patterns.
+# Source: https://www.unix.com/unix-for-dummies-questions-and-answers/115385-grep-v-option.html
 curl --location --silent $UNSW_COURSETABLE | sort | uniq | while read line
 do
-    # Inverse matching Selected lines are those not matching any of the specified patterns.
-    # Source: https://www.unix.com/unix-for-dummies-questions-and-answers/115385-grep-v-option.html
-    match_string=$(echo $line|egrep "^ *<td class=\"data\">.*\"$major[0-9][0-9][0-9][0-9]*.*\">.*</a>"|egrep -v ".*$major[0-9][0-9][0-9][0-9]*.*$major[0-9][0-9][0-9][0-9]*")
+    match_string=$(echo $line|egrep "^.*<td class=\"data\">.*\"$major[0-9][0-9][0-9][0-9]*.*\">.*</a>"|egrep -v ".*$major[0-9][0-9][0-9][0-9]*.*$major[0-9][0-9][0-9][0-9]*")
     # checking the brief structure of the online data
     # echo $match_string
     if [ "$match_string" == "" ]
