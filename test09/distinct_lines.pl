4ee419ad933610edf69ff61a92163df343fbab73
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-16 11:07:57 +1000

    Q3

--- test09/distinct_lines.pl
+++ test09/distinct_lines.pl
@@ -10,3 +10,13 @@ foreach $line(<STDIN>){
 	$line = uc($line);
     # delete the extra space
 	$line =~ s/ *//g ;
+	$words_dict{$line}++;
+	$distinct = scalar keys %words_dict;
+
+	if(lc($distinct) == lc($input_value)){
+		print "$input_value distinct lines seen after $counter lines read.\n";
+		exit 0;
+	}
+}
+
+print "End of input reached after $counter lines read - $input_value different lines not seen.\n";
