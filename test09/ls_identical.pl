cc01fbe0c1262b3c55ff2f6d5eece34113bb749c
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-16 16:29:49 +1000

    Q3

--- test09/ls_identical.pl
+++ test09/ls_identical.pl
@@ -4,12 +4,14 @@
 $file_directory = $ARGV[0];
 # the comparison directory --> target directory
 $comparison_directory = $ARGV[1];
-# filelist
+# using the glob function getting the file list
+# Source https://perldoc.perl.org/functions/glob
 @file_list = glob "$file_directory/*";
 # Using the file comparison to check whether 2 files are equal
 # The following link has the sample of compare
 use File::Compare;
 for $item (@file_list){
+    # getting the file
 	$file=$item=~s/.*\///gr;
     # if compare return is not 0 files are not same ---> sample of using compare
     # Source: https://docstore.mik.ua/orelly/perl2/prog/ch32_19.htm
