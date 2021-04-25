5ede1608aabcc3bcf368f40c2e49ce7f62c831a1
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-25 18:41:44 +1000

    speed

--- Ass2/speed.pl
+++ Ass2/speed.pl
@@ -126,7 +126,7 @@ sub parse_command_line {
                 next;
             }
             # Checking p option in subset0
-            if ( $command_in_list =~ /([0-9]+)p$/ || $command_in_list =~ /\/((..+)*)\/p$/){
+            if ( $command_in_list =~ /([0-9]+)p$/ || $command_in_list =~ /\/((.+)*)\/p$/){
                 my $matches = $1;
                 my $number_matches_pointer = 1;
                 if ($command_in_list !~ /\//){
@@ -160,7 +160,7 @@ sub parse_command_line {
             }
             # This is for the q option in subset 0
             # Some of these codes are similar to p option in the previous part
-            if ( $command_in_list =~ /([0-9]+)q$/ || $command_in_list =~ /\/((..+)*)\/q$/){ #quit
+            if ( $command_in_list =~ /([0-9]+)q$/ || $command_in_list =~ /\/((.+)*)\/q$/){ #quit
                 my $matches = $1;
                 my $number_matches_pointer = 1;
                 if ($command_in_list !~ /\//){
@@ -323,7 +323,7 @@ sub parse_command_line {
             # The first part is to match the digital eg seq 1 5 | 2041 speed '4d'
             # The second part is match // eg seq 11 20 | 2041 speed '/[2468]/d'
             # This regrex is similar to the previous -p
-            elsif ( $command_in_list =~ /([0-9]+)d$/  || $command_in_list =~ /\/((..+)*)\/d$/){
+            elsif ( $command_in_list =~ /([0-9]+)d$/  || $command_in_list =~ /\/((.+)*)\/d$/){
                 my $matches = $1;
                 $match_pattern = qr/$matches/;
                 # Checking the matches result is number or not, defualt is number
