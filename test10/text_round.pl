7cb6207296315649476693d3a0dfcf584d50bb36
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-28 20:35:00 +1000

    changing EOF error

--- test10/text_round.pl
+++ test10/text_round.pl
@@ -6,16 +6,15 @@ foreach $line(<STDIN>){
     chomp $line;
     # The first situation x.[0-4]x
     # less than 4 --> omit
-    while($line){
-        if ($line =~ /((\d*)\.[0-4]\d*)/){
+    while($line =~ /((\d*)\.[0-4]\d*)/){
         # Getting the first matching line
         $current_number = $1;
         # Getting the second matching line
         $round_number = $2;
         $line =~ s/$current_number/$round_number/;
         }
-        # The situation which is greater than 4
-        if ($line =~ /((\d*)\.[5-9]\d*)/){
+    # The situation which is greater than 4
+    while($line =~ /((\d*)\.[5-9]\d*)/){
         # Getting the first matching line
         $current_number = $1;
         # Getting the second matching line
