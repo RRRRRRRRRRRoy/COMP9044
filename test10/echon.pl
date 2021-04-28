26f4d65702878c7c912ff5250a78b8d7c1a94a86
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-28 21:26:34 +1000

    testing echon

---
+++ test10/echon.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+
+$length_input = @ARGV;
+
+# This step is to checking the first value is number
+# And the second value is String
+
+if($length_input == 2){
+    if ($ARGV[0] =~ /^\d+$/ and $ARGV[1] =~ /^[a-zA-Z0-9]*$/){
+    # Setting the counter to counter the time of printings
+    $counter = 1;
+        # Doing the printing operation
+        while($counter <= $ARGV[0]){
+            print "$ARGV[1]\n";
+            $counter++;
+        }
+    }else{
+        # If the input is wrong
+        print "Usage: ./echon.pl <number of lines> <string>";
+        exit 1;
+    }
+}else{
+    # If the input is wrong
+    print "Usage: ./echon.pl <number of lines> <string>";
+    exit 1;
+}
