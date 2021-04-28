7b15622794d953df90a4e416b1822cca7769f414
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-04-28 20:31:01 +1000

    text round

--- test10/text_round.pl
+++ test10/text_round.pl
@@ -14,7 +14,16 @@ foreach $line(<STDIN>){
         $round_number = $2;
         $line =~ s/$current_number/$round_number/;
         }
-
+        # The situation which is greater than 4
+        if ($line =~ /((\d*)\.[5-9]\d*)/){
+        # Getting the first matching line
+        $current_number = $1;
+        # Getting the second matching line
+        $round_number = $2;
+        # Notice Here adding 1
+        $round_number =+ 1;
+        $line =~ s/$current_number/$round_number/;
+        }
     
     print "$line\n";
     }
