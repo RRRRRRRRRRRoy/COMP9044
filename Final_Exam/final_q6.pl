10048522c3a19ebd49165a3bb60acd68e96dd27c
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-05-06 16:40:59 +1000

    test Q6

--- Final_Exam/final_q6.pl
+++ Final_Exam/final_q6.pl
@@ -1,13 +1,22 @@
 #!/usr/bin/perl -w
 
 while($line=<STDIN>){
+    $separator = "\|";
     @ls_line = split(/\|/, $line);
-    #print $ls_line;
+    # print $ls_line;
     $name = $ls_line[2];
-    ($firstname , $lastname) = split(/, /, $name);
-    while($s =~ / $/){
-        $s =~ s/ $//;
+    # print $name;
+    $separator = ', ';
+    @name_list = split(/$separator/, $name);
+    $firstname = $name_list[0];
+    $lastname = $name_list[1];
+    #print $lastname;
+    #print $firstname;
+    while($lastname =~ / $/){
+        $regrex = ' $';
+        $lastname =~ s/ $//;
     }
-    $line =~ s/$f, $s/$s $f/;
+    # Swapping the name in the list
+    $line =~ s/$firstname, $lastname/$lastname $firstname/;
     print $line;
 }
