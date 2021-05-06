365a67b315617cf6c804d810b71a2cb1dfa62892
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-05-06 15:34:35 +1000

    Q3

--- Final_Exam/final_q3.sh
+++ Final_Exam/final_q3.sh
@@ -1,1 +1,17 @@
 #!/bin/dash 
+
+for directory in $(ls -d */)
+do
+    # echo $dir
+    # getting the file in the list
+    dir_list=$(ls "$directory/")
+    # Getting the number of files
+    dir_num=$(echo "$dir_list" | wc -l)
+    # echo $number
+    # Checking the number of the files
+    if test $dir_num -ge 2 
+    then
+       result=$(echo $directory | sed 's/\/$//g')
+       echo $result
+    fi
+done
