8c71ff775a01b171268caf21b8e64f9a3b71022b
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-05-06 13:50:07 +1000

    Q2

--- Final_Exam/final_q2.pl
+++ Final_Exam/final_q2.pl
@@ -2,9 +2,12 @@
 
 $counter = 0;
 
-while($line = <STDIN>){
+foreach $line (<STDIN>){
+    # This regrex is same as the final_q1
     $regrex = '^.+3711/.+$';
+    # Doing the compare
     if($line =~ /$regrex/){
+        # Adding the result to the counter
         $counter += 1;
     }
 }
