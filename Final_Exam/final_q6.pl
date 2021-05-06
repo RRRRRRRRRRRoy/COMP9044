825256d4f51a089c2061d9ab1c50efa304ddd4e4
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-05-06 16:10:16 +1000

    test Q5

--- Final_Exam/final_q6.pl
+++ Final_Exam/final_q6.pl
@@ -2,5 +2,12 @@
 
 while($line=<STDIN>){
     @ls_line = split(/\|/, $line);
-    print $ls_line;
+    #print $ls_line;
+    $name = $ls_line[2];
+    ($firstname , $lastname) = split(/, /, $name);
+    while($s =~ / $/){
+        $s =~ s/ $//;
+    }
+    $line =~ s/$f, $s/$s $f/;
+    print $line;
 }
