5e964dbabf03b49baa71b49ae4b582f5d9852d9f
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-05-06 15:05:48 +1000

    Q4

--- Final_Exam/final_q4.sh
+++ Final_Exam/final_q4.sh
@@ -2,13 +2,15 @@
 
 input1=$1
 input2=$2
+# Cutting the number from the string
 temp1=`echo  "$input1" | tr -cd "[0-9]" `
 temp2=`echo  "$input2" | tr -cd "[0-9]" `
 
 temporary=$temp1
+# Looping and printing
 while test $temporary -le $temp2
 do
-    # result=
+    # Printing the final result
     echo $(echo $input1 | sed s/$temp1/$temporary/g)
     temporary=$((temporary+1))
 done
